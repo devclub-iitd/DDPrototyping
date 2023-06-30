42e7e61fc3694fe626c9ff2acb5e77200617a0af
Santhosh Deshineni <97736785+FlashinAssassin1@users.noreply.github.com>
2023-06-30 23:05:48 +0530

    CockroachDB Updates (#5)
    * added master and client containers
    
    * added basic django app for configured for cockroachdb
    
    * added docker compose and removed pycache
    
    * readme
    
    * pycache
    
    * pycache
    
    * pycache
    
    * Update README.md
    
    * Update README.md
    
    * added changes for pr but psql not working
    
    * working with postgres engine but until psql running check not working
    
    * only adding script is left
    
    * commented OPTIONS
    
    * made separate docker composes
    
    * some changes for linux
    
    * some changes for linux

--- README.md
+++
@@ -1,5 +0,0 @@
-# DDPrototyping
-Distributed Docker Prototyping
-
-A repository to prototype a distributed storage setting for our production servers. We intend to utilize docker containers to emulate multiple VMs used in production.
-
---
+++ example-app-python-django/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+
+# until mysql -u $DB_USER -h $DB_HOST -D $DB_NAME -p $DB_PASSWORD -e ";" ; do
+#        echo "Can't connect " 
+#        sleep 5
+# done
+# echo "Mysql is up - executing command"
+
+until psql -U $DB_USER -h $DB_HOST -d $DB_NAME -p $DB_PORT -c "SELECT 1" >/dev/null 2>&1; do
+    echo "PostgreSQL is unavailable - sleeping"
+    sleep 5
+done
+
+echo "PostgreSQL is up - executing command"
+
+python ./cockroach_example/manage.py makemigrations cockroach_example
+python ./cockroach_example/manage.py migrate
+python ./cockroach_example/manage.py runserver 0.0.0.0:8080
+
+sleep infinity
